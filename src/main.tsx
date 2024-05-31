276367914a5883850bd1c95f9a159bedf6c3de08
Nguyen Vu <100701820+vu-dev2001@users.noreply.github.com>
2024-05-31 17:12:33 +0900

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
